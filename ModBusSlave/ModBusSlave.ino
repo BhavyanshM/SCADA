2129dc65be9a70012fe9b2868ea45c8b02876b4d
BhavyanshM <bhavyanshmishra@gmail.com>
2019-04-23 12:27:53 -0500

    Add the latest logger

--- ModBusSlave/ModBusSlave.ino
+++ ModBusSlave/ModBusSlave.ino
@@ -14,17 +14,17 @@ byte subnet[] = {255, 255, 255, 0};
 
 
 void monitoring(){
-  Mb.MBHoldingRegister[0] = 123; 
-  Mb.MBHoldingRegister[1] = 233; 
-  Mb.MBHoldingRegister[3] = 435;
+  Mb.MBHoldingRegister[0] = 234; 
+  Mb.MBHoldingRegister[1] = 345; 
+  Mb.MBHoldingRegister[3] = random(1000);
   
 }
 
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  Mb.begin("E10BAC", "3HW155W001F44", ip, gateway, subnet);
-//  Mb.begin("dlink-62EC", "nhvtg79881", ip, gateway, subnet);
+//  Mb.begin("E10BAC", "3HW155W001F44", ip, gateway, subnet);
+  Mb.begin("dlink-62EC", "nhvtg79881", ip, gateway, subnet);
   delay(1000);
 }
 
--- sync_modbus_tcp.py
+++ sync_modbus_tcp.py
@@ -1,10 +1,21 @@
 from pymodbus.client.sync import ModbusTcpClient
+import time
 
 UNIT = 0x00
 
 client = ModbusTcpClient('192.168.0.40')
-client.write_coil(1, True)
-rr = client.read_holding_registers(0, 8, unit=UNIT)
-result = client.read_coils(1,1)
-print(rr.registers)
+#client.write_coil(1, True)
+
+f = open("log.txt", 'a')
+f.write("\n----------------------------------------------------------------------\n\n"+time.ctime()+"\n\n")
+
+while True:
+	rr = client.read_holding_registers(0, 8, unit=UNIT)
+	#result = client.read_coils(1,1)
+	print(rr.registers)
+	time.sleep(0.1)
+	f.write(str(rr.registers)+"\n")
+
+f.close()
+
 client.close()
